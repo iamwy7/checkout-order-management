b3516e78894caef8533a72ac99c8ca21073df09c
Wythor <wythorbazan@gmail.com>
2025-04-04 14:24:57 -0300

    perf: change log method to not kill the dc adapter on error

--- orders/adapters/outputs/http_client/distribution_center_adapter.go
+++ orders/adapters/outputs/http_client/distribution_center_adapter.go
@@ -17,7 +17,7 @@ func (dca *DistributionCenterAdapter) GetDCsByItemId(itemID string) (*Distributi
 	// Prep request with path
 	httpReq, err := http.NewRequest("GET", path, nil)
 	if err != nil {
-		log.Fatalf("failed to prep request to get distribution centers for reason:%v", err.Error())
+		log.Printf("failed to prep request to get distribution centers for reason:%v", err.Error())
 		return nil, err
 	}
 
@@ -25,26 +25,26 @@ func (dca *DistributionCenterAdapter) GetDCsByItemId(itemID string) (*Distributi
 	client := &http.Client{}
 	httpResp, err := client.Do(httpReq)
 	if err != nil {
-		log.Fatalf("integration error to get distribution centers with status: %d and error %v", httpResp.StatusCode, err.Error())
+		log.Printf("integration error to get distribution centers with status: %d and error %v", httpResp.StatusCode, err.Error())
 		return nil, ErrIntegrationServer
 	}
 
 	// Close when done
 	defer httpResp.Body.Close()
 	if httpResp.StatusCode != http.StatusOK {
-		log.Fatalf("failed to get distribution centers by id: %v with status code: %d", itemID, httpResp.StatusCode)
+		log.Printf("failed to get distribution centers by id: %v with status code: %d", itemID, httpResp.StatusCode)
 		return nil, ErrIntegrationClient
 	}
 
 	// Decode the response
 	var dcResp DistributionCenterResponse
 	if err := json.NewDecoder(httpResp.Body).Decode(&dcResp); err != nil {
-		log.Fatalf("failed to decode response: %v", err.Error())
+		log.Printf("failed to decode response: %v", err.Error())
 		return nil, ErrIntegrationDecodeJson
 	}
 	// Check if the response is empty (that definetly means that the itemID is not valid or something else)
 	if len(dcResp.DistributionCenters) == 0 {
-		log.Fatalf("no distribution centers found for itemID: %v", itemID)
+		log.Printf("no distribution centers found for itemID: %v", itemID)
 		return nil, ErrIntegrationDCsEmpty
 	}
 	return &dcResp, nil
