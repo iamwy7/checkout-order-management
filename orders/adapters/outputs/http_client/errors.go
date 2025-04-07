c26438db0e1a0204a4e49222fe9965ca91b5593f
Wythor <wythorbazan@gmail.com>
2025-04-07 02:43:53 -0300

    feat: add errors to dc adapter

--- orders/adapters/outputs/http_client/errors.go
+++ orders/adapters/outputs/http_client/errors.go
@@ -6,5 +6,5 @@ var (
 	ErrIntegrationServer     = errors.New("integration error to get distribution centers, contact support")
 	ErrIntegrationClient     = errors.New("failed to get distribution centers")
 	ErrIntegrationDecodeJson = errors.New("failed to decode response")
-	ErrIntegrationDCsEmpty   = errors.New("no distribution centers found")
+	ErrIntegrationDCsEmpty   = errors.New("one or more products doenst have any distribution centers")
 )
