f4ffc37cda67244886d0cb035a94a6b94116dbbc
Wythor <wythorbazan@gmail.com>
2025-04-07 02:47:08 -0300

    feat: add get orders by id usecase

---
+++ orders/application/usecase/get_orders_by_id.go
@@ -0,0 +1,23 @@
+package usecase
+
+import (
+	out_ports "github.com/iamwy7/meli-challenge/orders/application/ports/outputs"
+	"github.com/iamwy7/meli-challenge/orders/application/usecase/dtos"
+)
+
+type GetOrderUseCase struct {
+	order_repo out_ports.OrderRepository
+}
+
+func NewGetOrderUseCase(order_repo out_ports.OrderRepository, dc_repo out_ports.DistributionCentersRepository) *GetOrderUseCase {
+	return &GetOrderUseCase{
+		order_repo: order_repo}
+}
+
+func (uc *GetOrderUseCase) Execute(orderId string) (*dtos.CreatedOrderOutputDto, error) {
+	order, err := uc.order_repo.GetOrderById(orderId)
+	if err != nil {
+		return nil, err
+	}
+	return MapOrderToOrderOutputDto(order), nil
+}
